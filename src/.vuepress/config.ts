b8341c6daef4d1490ff55fd7e3657adc613e9cdd
thun888 <thun888@qq.com>
2024-07-21 13:13:40 +0800

    add umami

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,7 +9,9 @@ export default defineUserConfig({
   description: "基于小米路由器API的展示面板",
 
   theme,
-
+  head: [
+    ['script', { src: "/js/a.js",meta : "noindex,nofollow",async: true}],
+  ],
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
