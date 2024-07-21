3d7d3cadcd2e5e4272fbd9459e948020a67f10ad
thun888 <2238342947@qq.com>
2024-07-21 16:00:35 +0000

    Deploying to gh-pages from @ Mirouterui/docs@022b71c02bfddde96b95bac8060e40c177e0f980 🚀

---
+++ js/a.js
@@ -0,0 +1,10 @@
+// 引入统计
+(function() {
+    var script = document.createElement('script');
+    script.src = 'https://seeyou.hzchu.top/seeyou.js';
+    script.defer = true;
+    script.dataset.websiteId = '480b3ab1-bebe-454a-a462-84eab7a9e381';
+    script.dataset.hostUrl = 'https://seeyou.hzchu.top';
+    script.dataset.domains = 'blog.hzchu.top';
+    document.head.appendChild(script);
+})();
